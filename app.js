5e070575f298116e46dcea30b8bbcd3ba434d9d0
Jacob Davis <jsdavis1010@gmail.com>
2017-04-18 17:28:11 -0700

    Fixed port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
+const PORT = process.env.PORT || 8080;
+
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log('Example app listening on port ' + PORT);
 });
