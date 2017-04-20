3769324c37e3d61c0db9a985fdb4610b2cb757fc
Reebal K Abelson <rabelson@ieng6-201.ucsd.edu>
2017-04-20 14:55:00 -0700

    added comments to foo.js

--- foo.js
+++ foo.js
@@ -1,3 +1,8 @@
+/**
+ * Represents the "foo" function.
+ * @param {string} bar - The string to compare with
+ * @returns {boolean} Whether bar is "baz"
+ */
 function foo(bar) {
   return (bar === "baz");
 }
