48ad99194a68e5f18cae3d5f4146c9eaaa9f296d
Jarnin Fang <jarninfang@gmail.com>
2017-04-21 21:26:45 -0700

    express change

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js',express.static(path.join(__dirname, 'public/js')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
