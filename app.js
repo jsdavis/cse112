84be7d4cf2a1dca345a69abd910be85d856f5775
Jarnin Fang <jarninfang@gmail.com>
2017-04-21 21:23:08 -0700

    revert

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
