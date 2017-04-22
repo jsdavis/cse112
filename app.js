6667efbe370fa57b0dfca3bff95fbd6e89463a16
Jarnin Fang <jarninfang@gmail.com>
2017-04-21 21:18:03 -0700

    isValidTime function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const PORT = process.env.PORT || 8080;
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,31 @@ function submit() {
   var output = foo(input);
   document.getElementById('test').innerHTML = output;
 }
+
+/*
+ * isValidTime takes in a string as the parameter and returns true or false
+ * depending on if it is of the format hh:mm where hh cannot be greater than 12
+ * and mm cannot be greater than 59
+ */
+function isValidTime(timeString) {
+  //Regular expression to match required time format
+  re = /^\d{1,2}:\d{2}([ap]m)?$/;
+
+  if(timeString.value != '' && !timeString.match(re)) {
+    console.log('Invalid time format');
+    return false;
+  } else  {
+    var hours = timeString.slice(0, 2);
+    var mins = timeString.slice(3, 5);
+    if(parseInt(hours) > 12 || parseInt(mins) > 59) {
+      console.log('Invalid time format');
+      return false;
+    }
+    console.log('Valid time format');
+    return true;
+  }
+}
+
+function formatTime(timeString) {
+
+}
