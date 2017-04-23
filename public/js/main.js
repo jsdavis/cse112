844bddb283bcfb54ceb1beff2f33f0dd277de4fc
Gabriel mechali <gabrielmechali@Gabriels-MacBook-Pro.local>
2017-04-23 16:33:28 -0700

    refactoring main.js + parsing format string

--- public/js/main.js
+++ public/js/main.js
@@ -20,9 +20,9 @@ function submitValidTime() {
  * Grabs submission details from the submit format HTML element
  */
 function submitFormatTime() {
-  var time_format = document.getElementById('time_format_string').value;
-  var output = formatTime(time_format);
-  document.getElementById('format_time_retval').innerHTML = output;
+  let timeFormat = document.getElementById('timeFormatString').value;
+  let output = formatTime(timeFormat);
+  document.getElementById('formatTimeRetval').innerHTML = output;
 }
 
 /**
@@ -33,21 +33,21 @@ function submitFormatTime() {
  * @return {boolean} False if invalid time format, True otherwise.
  */
 function isValidTime(timeString) {
-  if(isEmpty(timeString)){
+  if(isEmpty(timeString)) {
     console.log('Time input is empty');
     return false;
   }
-  console.log("starting is ValdiTime");
-  var time = getTime(timeString);
-  var date = getDate(timeString);
+  console.log('starting is ValdiTime');
+  let time = getTime(timeString);
+  let date = getDate(timeString);
 
-  if(time==false){
-    console.log("time is false");
+  if(time==false) {
+    console.log('time is false');
     return false;
   }
 
-  if(date==false){
-    console.log("date is false");
+  if(date==false) {
+    console.log('date is false');
   }
 
     return true;
@@ -55,96 +55,106 @@ function isValidTime(timeString) {
 
 /**
  * Formats Time based on the inputted hours, minutes, seconds, and milliseconds
- * @param {string} hour The hour to format
- * @param {string} minute The minute to format
- * @param {string} second The second to format
- * @param {string} millisecond The millisecond to format
+ * @param {string} string to parse.
  * @return {string} valid time, "This is an invalid time." if fails
  */
 function formatTime(string) {
-  var time, date;
-  if(!isValidTime(string)){
+  let time;
+  let date;
+  if(!isValidTime(string)) {
     return false;
-  }
-  else {
+  } else {
     time = getTime(string);
     date = getDate(string);
   }
 
-  var format = (string.split(" - "))[1];
-  if(isEmpty(format) || format == null){
+  let format = (string.split(' - '))[1];
+  if(isEmpty(format) || format == null) {
     return time.original;
-  }
-  else {
-    format = format.replace(/%/g, "");
-    format = format.replace(/ /g, "");
-    var formatArr = format.split("");
+  } else {
+    format = format.replace(/%/g, '');
+    format = format.replace(/ /g, '');
+    let formatArr = format.split('');
     format = manipulateString(time, date, formatArr);
   }
 }
 
-function manipulateString(time, date, arr){
-  if(time == false || date == false){
-    console.log("Input is invalid");
+/**
+ * Formats Time based on the inputted hours, minutes, seconds, and milliseconds
+ * @param {object} time object
+ * @param {object} date object
+ * @param {object} arr all flags for format
+ * @return {string} valid time, "This is an invalid time." if fails
+ */
+function manipulateString(time, date, arr) {
+  if(time == false || date == false) {
+    console.log('Input is invalid');
     return false;
   }
 
-  for(var i=0; i<arr.legnth; i++){
-    switch(arr[i]){
-      case "a": break;
-      case "b": break;
-      case "c": break;
-      case "d": break;
+  for(let i=0; i<arr.legnth; i++) {
+    switch(arr[i]) {
+      case 'a': break;
+      case 'b': break;
+      case 'c': break;
+      case 'd': break;
     }
   }
-
-
 }
 
 
-
-
-
-function isEmpty(string){
-  if(string == ""){
+/**
+ * Checks whether string is empty
+ * @param {string} string to check
+ * @return {boolean} true or false
+ */
+function isEmpty(string) {
+  if(string == '') {
     return true;
   }
   return false;
 }
 
-function getDate(string){
-  if(isEmpty(string)){
+/**
+ * parses string to date object
+ * @param {string} string to parse
+ * @return {object} returns date boject
+ */
+function getDate(string) {
+  if(isEmpty(string)) {
     return false;
   }
 
-  dateFormat = /(([0]?[0-9]|1[0-2])\/([0-2]?[0-9]|3[0-1])\/([0-9]{2}?[0-9]{2})?)/;
+  dateFormat=/(([0]?[0-9]|1[0-2])\/([0-2]?[0-9]|3[0-1])\/([0-9]{2}?[0-9]{2})?)/;
 
-  if(!string.match(dateFormat)){
-    console.log("no matches");
+  if(!string.match(dateFormat)) {
+    console.log('no matches');
     return false;
-  }
-  else {
+  } else {
+      let date = (string.match(dateFormat))[0];
+      let dateArr = date.split('/');
 
-      var date = (string.match(dateFormat))[0];
-      var dateArr = date.split("/");
+      let month = dateArr[0];
+      let day = dateArr[1];
+      let year = dateArr[2];
+      console.log('found matches'+day);
 
-      var month = dateArr[0];
-      var day = dateArr[1];
-      var year = dateArr[2];
-      console.log("found matches"+day);
-
-      if(parseInt(month) > 12 || parseInt(day) > 31){
-        console.log("month: "+month+ "day: "+day);
+      if(parseInt(month) > 12 || parseInt(day) > 31) {
+        console.log('month: '+month+ 'day: '+day);
         return false;
       }
-      return {month: month, day: day, year: year, weekday: null, timezone: null};
+      return {month: month, day: day, year: year,
+        weekday: null, timezone: null};
     }
-    
 }
 
-
-function getTime(string){
-  if(isEmpty(string)){
+/**
+ * parses string to time object
+ * @param {string} string to parse
+ * @return {object} returns time boject
+ */
+function getTime(string) {
+  if(isEmpty(string)) {
     return false;
   }
   timeFormat = /([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9](:([0-9]{4}))?)?/;
@@ -152,30 +162,34 @@ function getTime(string){
   if(!string.match(timeFormat)) {
     console.log('Time input is invalid');
     return false;
-  }
-  else {
-    var addS = false, addM = false;
-    var time = (string.match(timeFormat))[0];
+  } else {
+    let addS = false;
+    let addM = false;
+    let time = (string.match(timeFormat))[0];
 
-    let timeArr = time.split(":");
+    let timeArr = time.split(':');
     console.log(timeArr);
     let hours = timeArr[0];
     let mins = timeArr[1];
-    var seconds = 0, milliseconds = 0;
-    if(timeArr.length > 2){
+    let seconds = 0;
+    let milliseconds = 0;
+
+    if(timeArr.length > 2) {
       addS = true;
       seconds = timeArr[2];
     }
-    if(timeArr.length == 4){
+    if(timeArr.length == 4) {
       addM = true;
       milliseconds = timeArr[3];
     }
 
-    if(parseInt(hours) > 24 || parseInt(mins) > 59 || (addS && parseInt(seconds) > 59) || (addM && parseInt(milliseconds))){
+    if(parseInt(hours) > 24 || parseInt(mins) > 59 ||
+      (addS && parseInt(seconds) > 59) ||(addM && parseInt(milliseconds))) {
       console.log('Invalid time format');
       return false;
     }
-    return {hours: hours, minutes: mins, seconds: seconds, milliseconds: milliseconds, original: time};
+    return {hours: hours, minutes: mins, seconds: seconds,
+      milliseconds: milliseconds, original: time};
   }
 }
 
