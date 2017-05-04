3d9c08b4443ca2fe383f5617828b5b62dec3b984
Jacob Davis <jsdavis1010@gmail.com>
2017-05-03 17:42:03 -0700

    Fixed eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": ["eslint:recommended", "google"],
+    "env": {
+      "browser": true,
+      "es6": true,
+      "jquery": true,
+      "mocha": true,
+      "node": true
+    },
+    "rules": {}
+};
