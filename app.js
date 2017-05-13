93938471b70faa375b0b549baf01d6bf71e6e500
Jarnin Fang <jarninfang@gmail.com>
2017-05-12 18:26:27 -0700

    Connected to online hosted mLab database

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.use('/js', express.static(path.join(__dirname, 'public/js')));
 /*
  *  Connect to MongoDB
  */
-mongoose.connect(config.mongoDBUrl);
+if(process.env.NODE_ENV === "production") 
+	mongoose.connect("mongodb://unluckygeniuses:password@ds137891.mlab.com:37891/unluckygeniuses");
+else
+	mongoose.connect(config.mongoDBUrl);
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-	console.log('Connected to mongolab');
+  console.log('Connected to mongodb');
 });
 
 app.get('/', (req, res) => {
