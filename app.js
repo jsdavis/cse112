f19c3046374077849bc07def80525937dae893e7
Jarnin Fang <jarninfang@gmail.com>
2017-05-12 17:14:19 -0700

    Connected to local mongoDB database

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./server/config/config');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
+
+/*
+ *  Connect to MongoDB
+ */
+mongoose.connect(config.mongoDBUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  //console.log('Connected to mongolab');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
