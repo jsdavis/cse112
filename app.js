c3d47b4e1d05921d2658acd44fcca8e4e8f61f29
Jarnin Fang <jarninfang@gmail.com>
2017-05-12 17:14:47 -0700

    Connected to local mongoDB database

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(config.mongoDBUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  //console.log('Connected to mongolab');
+	console.log('Connected to mongolab');
 });
 
 app.get('/', (req, res) => {
