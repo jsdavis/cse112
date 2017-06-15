a747361c257ba3ad8ce7963dc1791b0d5df71d35
Jacob Davis <jsdavis1010@gmail.com>
2017-06-15 13:59:42 -0700

    Killed the stupid socket once and for all

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -13,8 +13,6 @@ let myCompanyId;
 
 $(document).ready(() => {
   $('.parallax').parallax();
-  const socket = io(); // Initialize Socket
-  // Socket variables
   // var CONNECTION = "connection";
   const VALIDATE_COMPANY_ID = 'validate_company_id';
   const VISITOR_LIST_UPDATE = 'visitor_list_update';
@@ -31,16 +29,6 @@ $(document).ready(() => {
   updateVisitors();
   updateModalVisitor();
 
-  // alert("socket  on");
-  // Update Patient List
-  // socket.on(VALIDATE_COMPANY_ID, (socket) => {
-    // alert("in socket on");
-  //   socket.on(VISITOR_LIST_UPDATE, (data) => {
-  //     const compiledHtml = template(data);
-  //     $('#visitor-list').html(compiledHtml);
-  //   });
-  // });
-
   /** *
   * Function Listener for Opening a Modal
   */
@@ -53,22 +41,12 @@ $(document).ready(() => {
     const visitorData = getVisitorData(myCompanyId, uniqueId);
     const compiledTemplate = modalTemplate(visitorData);
     $('.modal-dialog').html(compiledTemplate);
-    // socket.on(VALIDATE_COMPANY_ID, (socket) => {
-    //   socket.emit('send Id', uniqueId);
-    //   socket.on('send visitorData', (data) => {
-    //     const compiledTemplate = modalTemplate(data);
-    //     $('.modal-dialog').html(compiledTemplate);
-    //   });
-    // });
   });
 
   $(document).on('click', '.check-in-btn', function() {
     /* eslint-disable */ // eslint doesn't like 'this'
     const id = $(this).closest('.modal-content').find('.phone-number').attr('value');
     /* eslint-enable */
-    socket.on(VALIDATE_COMPANY_ID, (socket) => {
-      socket.emit('check-in-patient', id);
-    });
   });
 
   /* eslint-disable */ // eslint doesn't like 'this'
