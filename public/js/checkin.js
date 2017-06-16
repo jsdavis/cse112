cd96c1a67e47df69180dacd9dd06c87eee3284e8
Jacob Davis <jsdavis1010@gmail.com>
2017-06-15 21:17:55 -0700

    Fixed checkin

--- public/js/checkin.js
+++ public/js/checkin.js
@@ -1,11 +1,22 @@
 $(document).ready(() => {
-  const companyData = JSON.parse(localStorage.getItem('currentCompany'));
+  const socket = io();
 
-   /* eslint-disable */
-  $('#submit-btn').click(function(e) {
+  const VALIDATE_COMPANY_ID = 'validate_company_id';
+  const ADD_VISITOR = 'add_visitor';
+
+  const companyData = JSON.parse(localStorage.getItem('currentCompany'));
+  console.log(companyData);
+  socket.emit(VALIDATE_COMPANY_ID, companyData);
+
+    // Prevent users from scrolling around on iPad
+  document.ontouchmove = function(e) {
+    e.preventDefault();
+  };
+
+  /* eslint-disable */
+  $('.check-in').on('submit', function(e) {
       e.preventDefault();
       submitForm();
-      checkIn();
       $('#clock').removeClass('hide');
       $('#clock').addClass('show');
       $('#tap-to-check').removeClass('hide');
@@ -14,31 +25,19 @@ $(document).ready(() => {
       $(this).addClass('hide');
 
   });
-  /* eslint-enable */
+  $('#tap-to-check').on('click', function(e) {
+      e.preventDefault();
+      startCheckIn();
+      $(this).removeClass('show');
+      $(this).addClass('hide');
+      $('.check-in').removeClass('hide');
+      $('.check-in').addClass('show');
+      $('#first_name').val('');
+      $('#last_name').val('');
+      $('#email').val('');
 
-  function checkIn() {
-    $.post({
-      type: 'POST',
-      async: false,
-      data: {
-        employee_id: localStorage.getItem(currentUser._id),
-        customer_first_name: $('#first_name').val(),
-        customer_last_name: $('#last_name').val(),
-        customer_email: $('email').val(),
-      },
-      url: '/api/appointments/checkin/',
-      dataType: 'json',
-      success: function(response) {
-        if (response.statusCode !== 200)
-          alert('Checkin failed!');
-        else {
-          $('#first_name').val('');
-          $('#last_name').val('');
-          $('#email').val('');
-        }
-      },
-    });
-  }
+  });
+  /* eslint-enable */
 
     // Bind Listeners
   // $('#tap-to-check').on('click', startCheckIn);
@@ -75,6 +74,7 @@ $(document).ready(() => {
              }
         );
     }
+    socket.emit(ADD_VISITOR, data);
 
 
     /* eslint-disable */ // eslint doesn't like 'this'
@@ -84,6 +84,43 @@ $(document).ready(() => {
       opacity: '0',
     }, 0);
   }
+    // Grabs elements from the check in and puts it into an object
+  function grabFormElements() {
+    const newVisitor = {};
+    if(companyData==null) {
+      return null;
+    }
+    newVisitor.company_id = companyData._id;
+    newVisitor.first_name= $('#first_name').val();
+    newVisitor.last_name = $('#last_name').val();
+    newVisitor.phone_number = $('#email').val();
+    newVisitor.checkin_time = new Date();
+    return newVisitor;
+  }
+
+    // CLOCK
+  function updateClock() {
+    const currentTime = new Date( );
+    let currentHours = currentTime.getHours( );
+    let currentMinutes = currentTime.getMinutes( );
+        // var currentSeconds = currentTime.getSeconds ( );
+        // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = ( currentMinutes < 10 ? '0' : '' ) + currentMinutes;
+        // currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+        // Convert the hours component to 12-hour format if needed
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+        // Convert an hours component of "0" to "12"
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+        // Compose the string for display
+    const currentTimeString = currentHours + ':' + currentMinutes;
+
+    $('#clock').html(currentTimeString);
+  }
+  updateClock();
+  setInterval(updateClock, 60 * 1000);
 
   // Find a specific cookie name
   function getCookie(cName) {
