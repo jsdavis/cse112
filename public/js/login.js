dc40ecae64dce708284d5a1f7da0127e5b559119
Jacob Davis <jsdavis1010@gmail.com>
2017-06-15 21:37:32 -0700

    Removed alert

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,6 @@ $('#form_login').submit((event) => {
 });
 $('#loginButton').click(() => {
   const userData = grabUserData();
-  alert(JSON.stringify(userData));
   ajaxPostUserEmployee(userData);
 });
 
